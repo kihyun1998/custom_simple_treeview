cb1a778c193de49c9aa022cda806090187bebc07
박기현 <kihyun1998@naver.com>
2024-05-14 12:58:47 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# custom_simple_treeview
+ This is Custom Simple Treeview in flutter
---
+++ lib/custom_simple_treeview.dart
@@ -0,0 +1,7 @@
+library custom_simple_treeview;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
